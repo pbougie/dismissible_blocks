1c894a3a90339ccc437097f846125f12372a7aef
Patrick Bougie <patrick@bougie.ca>
2014-12-22 12:34:14 -0500

    Explicitly load DismissibleBlocksHelper

--- lib/dismissible_blocks/engine.rb
+++ lib/dismissible_blocks/engine.rb
@@ -4,5 +4,8 @@ require 'dismissible_blocks/version'
 
 module DismissibleBlocks
   class Engine < ::Rails::Engine
+    config.to_prepare do
+      ApplicationController.helper(DismissibleBlocksHelper)
+    end
   end
 end
