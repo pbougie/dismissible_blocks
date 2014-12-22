f7235c84aaf6f3b6a59b1375b368101b859b919e
Patrick Bougie <patrick@bougie.ca>
2014-12-22 12:32:53 -0500

    Explicitly require source files

--- lib/dismissible_blocks.rb
+++ lib/dismissible_blocks.rb
@@ -1,13 +1,8 @@
 require 'dismissible_blocks/engine'
 
 module DismissibleBlocks
-  extend ActiveSupport::Autoload
-
   class ModelUnavailable < StandardError; end
   class AttributeUnavailable < StandardError; end
   class AttributeNotArray < StandardError; end
   class ContentMissing < StandardError; end
-
-  autoload :Controller
-  autoload :Helper
 end
--- lib/dismissible_blocks/engine.rb
+++ lib/dismissible_blocks/engine.rb
@@ -1,3 +1,7 @@
+require 'dismissible_blocks/controller'
+require 'dismissible_blocks/helper'
+require 'dismissible_blocks/version'
+
 module DismissibleBlocks
   class Engine < ::Rails::Engine
   end
