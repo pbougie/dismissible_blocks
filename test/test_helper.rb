f55969ee7455de5bbbab3f7d484dd92718f3d22d
Patrick Bougie <patrick@bougie.ca>
2014-12-22 13:08:57 -0500

    Update test configurations
    Update config for Rails 4.2.0 engine.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rails/test_help'
 
-Rails.backtrace_cleaner.remove_silencers!
-ActiveRecord::Migrator.migrate File.expand_path('../dummy/db/migrate', __FILE__)
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
 ActiveSupport::TestCase.fixtures :all
