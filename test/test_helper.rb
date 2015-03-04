3e68526c7cfacd575f254479ba1bbcf7cf185491
Patrick Bougie <patrick@bougie.ca>
2015-03-04 15:32:49 -0500

    Add MiniTest Pride coloured output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
 
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
